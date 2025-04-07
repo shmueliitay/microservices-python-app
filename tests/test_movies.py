b375815141e13afc9f9f67a20f7e9b7e21e3e0f8
root <shmueliitay@gmail.com>
2025-04-07 17:05:05 +0300

    try 16

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -4,7 +4,7 @@ import requests
 
 class TestMoviesService(unittest.TestCase):
     def setUp(self):
-        self.url = "http://cinema.com:5001/movies"
+        self.url = "https://cinema.com/movies"
 
     def test_all_movie_records(self):
         """ Test /movies/<movieid> for all known movies"""
